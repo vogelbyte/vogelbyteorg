20bd9cc40635358c6bbabb8fcf96c298ec175d6e
dheeraj7477 <dheeraj.vogelbyte@gmail.com>
2019-04-15 15:02:40 +0530

    login code

---
+++ Login/src/main/java/com/Login/app/entity/Admin.java
@@ -0,0 +1,56 @@
+package com.Login.app.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+
+@Entity
+@Table (name="admin")
+public class Admin {
+
+	@Id
+	@GeneratedValue( strategy = GenerationType.AUTO)
+	@Column(name="person_id")
+	private int id;
+	
+	@Column(name="email")
+	private String email;
+	
+	@Column(name="password")
+	private String password;
+
+	@Override
+	public String toString() {
+		return "Admin [id=" + id + ", email=" + email + ", password=" + password + "]";
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+	
+}
