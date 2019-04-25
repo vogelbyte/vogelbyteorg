99a4f17c6c9d3eef3da1d4abe31996e55eee17d9
rohit8802 <rohit.vogelbyte@gmail.com>
2019-04-25 11:39:47 +0530

    today

--- Company/IMS/src/app/login/login.component.ts
+++ Company/IMS/src/app/login/login.component.ts
@@ -2,6 +2,10 @@ import { Component, OnInit } from '@angular/core';
 import {Router} from '@angular/router';
 import {MatDialog} from '@angular/material'
 import { HttpClient } from '@angular/common/http';
+import { FormGroup } from '@angular/forms';
+import { Admin } from '../admin';
+import { ServiceService } from '../service.service';
+import { AlertService } from '../alert.service';
 
 @Component({
   selector: 'app-login',
@@ -32,31 +36,30 @@ export class LoginComponent implements OnInit {
     //       username: ['', Validators.required],
     //       password: ['', Validators.required]
     //   });
-    private router: Router,
-    private http: HttpClient
-    ) { }
-username: string;
-password: string;
-  ngOnInit() {
-  }
-  login() : void {
+    
+     } 
+// username: string;
+// password: string;
+  // ngOnInit() {
+  // }
+  // login() : void {
 
-    let body = {
-        "email":"dheeraj@gmail.com",
-        "password":"5252"
-        };
-   this.http.post("http://localhost:8082/api/admins/login",body).subscribe((data:any)=>{
-      console.log("this is here ==>",data);
-   });
+  //   let body = {
+  //       "email":"dheeraj@gmail.com",
+  //       "password":"5252"
+  //       };
+  //  this.http.post("http://localhost:8082/api/admins/login",body).subscribe((data:any)=>{
+  //     console.log("this is here ==>",data);
+  //  });
    
 
-    if(this.username == 'admin' && this.password == 'admin'){
-     this.router.navigate(["user"]);
-    }else {
-      alert("Invalid credentials");
-    }
+  //   if(this.username == 'admin' && this.password == 'admin'){
+  //    this.router.navigate(["user"]);
+  //   }else {
+  //     alert("Invalid credentials");
+  //   }
 
-    onSubmit() {
+    onsubmit() {
       // this.submitted = true;
       this.serviceService.login(this.userModel)
       .subscribe(
