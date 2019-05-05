3514e325acfb3026b49625c95345f6f2cde99ee8
rohit8802 <rohit.vogelbyte@gmail.com>
2019-05-05 12:52:29 +0530

    bill table

--- Company/IMS/src/app/tickets/tickets.component.ts
+++ Company/IMS/src/app/tickets/tickets.component.ts
@@ -7,12 +7,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TicketsComponent implements OnInit {
 
-  data:any;
+  // data:any;
 
   getRandomColor() {
     var color = Math.floor(0x1000000 * Math.random()).toString(16);
     return '#' + ('000000' + color).slice(-6);
     }
+
+  
+  //function to get random colors
+  // public getRandomColor(){
+  //     var letters = '0123456789ABCDEF'.split('');
+  //     var color = '#';
+  //     for (var i = 0; i < 6; i++){
+  //         color += letters[Math.floor(Math.random() * 16)];
+  //     }
+  //     return color;
+  // }
+  
 
   enum_details = [
     {name: 'static'},
@@ -27,8 +39,8 @@ export class TicketsComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
-      this.data =  this.getRandomColor();
-      console.log("COLOR==>",this.data);
+      // this.data =  this.getRandomColor();
+      // console.log("COLOR==>",this.data);
   }
 
   
