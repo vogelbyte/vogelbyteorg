7c931fad1347020c8715ac4cbbabc146cb8ac948
dheeraj7477 <dheeraj.vogelbyte@gmail.com>
2019-05-15 11:04:25 +0530

    1

---
+++ IMS/src/app/app.module.ts
@@ -0,0 +1,43 @@
+import { BrowserModule } from '@angular/platform-browser';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { NgModule } from '@angular/core';
+import { CustomMaterialModule } from './material.module';
+import { AppRoutingModule } from './app-routing.module';
+import {FormsModule} from '@angular/forms';
+import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { UserComponent } from './user/user.component';
+import { HttpClientModule } from '@angular/common/http';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { BillComponent } from './bill/bill.component';
+import { NotesComponent } from './notes/notes.component';
+import { ResizableModule } from 'angular-resizable-element';
+import { TicketsComponent } from './tickets/tickets.component';
+import { ScrollToModule } from '@nicky-lenaers/ngx-scroll-to';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LoginComponent,
+    UserComponent,
+    DashboardComponent,
+    BillComponent,
+    NotesComponent,
+    TicketsComponent,
+    TicketsComponent
+  ],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    CustomMaterialModule,
+    FormsModule,
+    AppRoutingModule,
+    HttpClientModule,
+    ResizableModule,
+    ScrollToModule.forRoot(),
+    
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
